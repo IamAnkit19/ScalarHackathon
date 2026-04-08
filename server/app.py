443750944f3c04d3c2ec020cd94f9a60e46e9281
IamAnkit19 <ankit003322@gmail.com>
2026-04-08 22:00:58 +0530

    Changes in file

--- server/app.py
+++ server/app.py
@@ -1,3 +1,8 @@
 from app import app
 
-# This exposes your Flask app to OpenEnv
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
